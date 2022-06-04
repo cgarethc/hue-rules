2239c6920fd461ea376e821833f0bb72ea303fb5
cgarethc <gareth@cronin.co.nz>
2022-06-04 15:40:15 +1200

    Basic on/off and brightness working

--- config.example.js
+++ config.example.js
@@ -4,3 +4,49 @@ exports.bridge = {
   port: 80,                     // Optional
   timeout: 15000,               // Optional, timeout in milliseconds (15000 is the default)
 };
+
+// see https://github.com/cachecontrol/json-rules-engine
+// event types are "turnOn" and "turnOff"
+exports.rules = [
+  {
+    conditions: {
+      all: [
+        {
+          fact: 'Lounge lamp',
+          operator: 'equal',
+          value: true,
+          path: '$.on'
+        },
+        {
+          fact: 'Lounge light',
+          operator: 'equal',
+          value: false,
+          path: '$.on'
+        }
+      ]
+    },
+    event: {
+      type: 'on',
+      params: {
+        light: 'Lounge light'
+      }
+    }
+  },
+  {
+    conditions: {
+      any: [{
+        fact: 'Lounge light',
+        operator: 'equal',
+        value: true,
+        path: '$.on'
+      }]
+    },
+    event: {
+      type: 'brightness',
+      params: {
+        light: 'Lounge light',
+        brightness: 20
+      }
+    }
+  }
+];
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const huejay = require('huejay');
+const { Engine } = require('json-rules-engine');
 
 const bridgeConfig = require('./config').bridge;
+const rules = require('./config').rules;
 
 if (!bridgeConfig || !bridgeConfig.host || !bridgeConfig.username) {
   console.error('Please provide a host and username in config.js');
+  process.exit(2);
+}
+if (!rules || !Array.isArray(rules)) {
+  console.error('Please provide at least one rule in config.js');
   process.exit(2);
 }
 
@@ -11,6 +17,13 @@ if (!bridgeConfig || !bridgeConfig.host || !bridgeConfig.username) {
   const client = new huejay.Client(
     bridgeConfig
   );
+
+  const engine = new Engine();
+  for (let rule of rules) {
+    console.debug('Adding rule:', rule);
+    engine.addRule(rule);
+  }
+
 
   try {
     console.log('Testing connection...')
@@ -23,7 +36,36 @@ if (!bridgeConfig || !bridgeConfig.host || !bridgeConfig.username) {
   }
 
   const lights = await client.lights.getAll();
-  console.log(lights.map(light => light.name));
+  const facts = {};
+  for (let light of lights) {
+    facts[light.name] = {on: light.on, hue: light.hue, brightness: light.brightness};
+  }
+  console.log(facts);
+
+  const { events } = await engine.run(facts);
+  console.log('events', events);
+
+  for (let event of events) {
+    console.log(event);
+    if (event.type === 'on') {
+      const light = lights.find(light => light.name === event.params.light);
+      light.on = true;
+      await client.lights.save(light);
+    }
+    else if (event.type === 'off') {
+      const light = lights.find(light => light.name === event.params.light);
+      light.on = false;
+      await client.lights.save(light);
+    }
+    else if (event.type === 'brightness') {
+      const light = lights.find(light => light.name === event.params.light);
+      light.brightness = Math.max(0, Math.min(255, event.params.brightness));
+      await client.lights.save(light);
+    }
+    else{
+      console.warn('unrecognized event type', event.type);
+    }
+  }
 
 })();
 
